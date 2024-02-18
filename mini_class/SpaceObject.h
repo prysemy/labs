5f0cacb6d07b0cf8ccdd6487c89b19927e1eeddd
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-02-18 17:46:08 +0300

    Create SpaceObject.h

---
+++ mini_class/SpaceObject.h
@@ -0,0 +1,18 @@
+//
+// Created by amali on 18.02.2024.
+//
+
+#ifndef UNTITLED2_SPACEOBJECT_H
+#define UNTITLED2_SPACEOBJECT_H
+
+
+class SpaceObject {
+protected:
+    long radius;
+    unsigned long massa;
+public:
+    SpaceObject();
+};
+
+
+#endif //UNTITLED2_SPACEOBJECT_H
