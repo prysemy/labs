575377d1e13ed2582e303e97f88aab45eaf8fa6e
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2025-05-09 21:47:04 +0300

    Update 4.7.2.py

--- progs_for_labs/4.7.2/4.7.2.py
+++ progs_for_labs/4.7.2/4.7.2.py
@@ -2,13 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 m = np.arange(1, 9, 1)
-rm = np.array([28, 41, 49, 57, 64, 71, 77, 82])/10
+rm = np.array([2.7, 3.8, 4.7, 5.5, 6.0, 6.6, 7.2, 7.6])
 
 fig, ax = plt.subplots()
 fig.set_figwidth(12)
 fig.set_figheight(7)
-plt.plot(m, np.polyval(np.polyfit(m, rm ** 2, 1), m), linewidth=1, color='black')
-ax.errorbar(m, rm ** 2, yerr=np.array([0.4] * len(m)), fmt='o', elinewidth=1, capsize=3, capthick=0.5, markersize=2.5,
+k = np.polyfit(m, rm ** 2, 1)[0]
+print(k)
+er = np.array([.54, .76, .94, 1.1, 1.2, 1.32, 1.44, 1.52])
+plt.plot(m, np.polyval(np.polyfit(m, rm ** 2, 1), m), linewidth=1, color='black',
+         label=r'$k=(7.27\pm 0.39) см^2$')
+ax.errorbar(m, rm ** 2, yerr=er, fmt='o', elinewidth=1, capsize=3, capthick=0.5, markersize=2.5,
             color='black')
 plt.scatter(m, rm ** 2, s=np.array([8] * len(m)), color='black')
 ax.xaxis.set_ticks_position('bottom')
@@ -18,5 +22,6 @@ ax.grid(which="major", color='grey', linewidth=0.7, linestyle='--')
 ax.grid(which="minor", color='lightgrey', linewidth=0.7, linestyle='--')
 ax.set_ylabel(r'$r_m^2, см^2$')
 ax.set_xlabel('m')
+plt.legend()
 plt.savefig('graph1.png')
 plt.show()
